a8a46850950895783dd1c38461e6672b59dae98f
Md Abdur Rahim <dev.abdurrahim@outlook.com>
2025-09-22 21:24:56 +0600

    Generate Parentheses code proble solve

--- index.js
+++ index.js
@@ -1058,3 +1058,44 @@ const mergedList2 = mergeTwoLists(list5, list6);
 
 
 
+// TODO: Generate Parentheses
+
+// Given n pairs of parentheses, write a function to generate all combinations of well - formed parentheses.
+
+
+
+//   Example 1:
+
+// Input: n = 3
+// Output: ["((()))", "(()())", "(())()", "()(())", "()()()"]
+// Example 2:
+
+// Input: n = 1
+// Output: ["()"]
+
+
+// Constraints:
+
+// 1 <= n <= 8
+
+
+function generateParenthesis(n) {
+  const result = [];
+  const backtrack = (current, open, close) => {
+    if (current.length === n * 2) {
+      result.push(current);
+      return;
+    }
+    if (open < n) {
+      backtrack(current + "(", open + 1, close);
+    }
+    if (close < open) {
+      backtrack(current + ")", open, close + 1);
+    }
+  };
+  backtrack("", 0, 0);
+  return result;
+}
+// Example usage:
+console.log(`generateParenthesis:`, generateParenthesis(3)); // ["((()))", "(()())", "(())()", "()(())", "()()()"]
+console.log(`generateParenthesis:`, generateParenthesis(1)); // ["()"]
